c585e98c91a8b6f571e4667f266f5df942d1c51c
Daniel Puscher <dev@daniel-puscher.de>
2026-04-03 02:23:30 +0200

    chore: rename format to lint and add lint:check to CI

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,7 +1,7 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-max-line-length": [0, "always", Infinity],
-    "footer-max-line-length": [0, "always", Infinity],
+    "body-max-line-length": [0, "always", Number.POSITIVE_INFINITY],
+    "footer-max-line-length": [0, "always", Number.POSITIVE_INFINITY],
   },
 };
